52634da0af9acb6821d799073cf1132e5a1addfb
quophyie <quophyie@yahoo.com>
2019-03-18 23:53:07 +0000

    Updated Application entry point (i.e. UsersApplication.java) with de.invesdwin.instrument.DynamicInstrumentationLoader Updated docker-compose yml with defaults for env vars WEB_APP_PORT and DB_PORT_EXPOSED_ON_HOST Added  - script/build_run_docker_container.sh  - script/variables.sh
     Deleted build_docker_container.sh

--- src/main/java/com/quantal/exchange/users/UsersApplication.java
+++ src/main/java/com/quantal/exchange/users/UsersApplication.java
@@ -1,16 +1,20 @@
 package com.quantal.exchange.users;
 
+import de.invesdwin.instrument.DynamicInstrumentationLoader;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.zalando.tracer.spring.TracerAutoConfiguration;
 
 @SpringBootApplication
 @EnableScheduling
 //@ImportAutoConfiguration(TracerAutoConfiguration.class)
 public class UsersApplication {
+
+	static {
+		//Starts the aspectj weaver so that we can weave the compile time aspects
+		DynamicInstrumentationLoader.waitForInitialized(); //dynamically attach java agent to jvm if not already present
+		DynamicInstrumentationLoader.initLoadTimeWeavingContext(); //weave all classes before they are loaded as beans
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
